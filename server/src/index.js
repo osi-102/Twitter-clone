d8b47b9eed36fb75b2a9f6ac586a572668869933
Mayankchauhan <2003mayankchauhan.b@gmail.com>
2023-09-15 20:25:37 +0530

    fc

---
+++ server/src/index.js
@@ -0,0 +1,39 @@
+const logger = require('./utils/logger');
+const app = require('./app');
+const config = require('./config/keys');
+const { connectDB } = require('./config/db');
+
+let server;
+
+connectDB().then(() => {
+  logger.info('Connected to MongoDB');
+  server = app.listen(config.port, () => {
+    logger.info(`Listening on port ${config.port}`);
+  });
+});
+
+const exitHandler = () => {
+  if (server) {
+    server.close(() => {
+      logger.info('Server closed');
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = (error) => {
+  logger.error(error);
+  exitHandler();
+};
+
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
+
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM received');
+  if (server) {
+    server.close();
+  }
+});
